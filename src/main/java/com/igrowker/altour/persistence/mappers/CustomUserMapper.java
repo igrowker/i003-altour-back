bfd3a328176e01144e5b97cf7d30f11fac76ce22
pbl99 <pabloam1999@hotmail.com>
2024-10-04 10:47:52 +0200

    Se añade para recomendaciones de destinos y fix decrypt profile y @transactional para users

--- src/main/java/com/igrowker/altour/persistence/mappers/CustomUserMapper.java
+++ src/main/java/com/igrowker/altour/persistence/mappers/CustomUserMapper.java
@@ -2,6 +2,7 @@ package com.igrowker.altour.persistence.mappers;
 
 import com.igrowker.altour.dtos.internal.User.UserReadDTO;
 import com.igrowker.altour.persistence.entity.CustomUser;
+import com.igrowker.altour.persistence.entity.Place;
 import com.igrowker.altour.persistence.entity.VenueType;
 import com.igrowker.altour.utils.AESUtils;
 
@@ -25,6 +26,15 @@ public class CustomUserMapper {
          decryptedVenueType.setVenueType(aesUtils.decrypt(venueType.getVenueType())); 
          return decryptedVenueType;
      }).collect(Collectors.toSet());
+   	 
+   	 Set<Place> decryptedFavorites = user.getFavorites().stream().map(favorite -> {
+         Place decryptedFavorite = new Place();
+         decryptedFavorite.setId(favorite.getId());
+         decryptedFavorite.setExternalAPI(favorite.getExternalAPI());  
+         decryptedFavorite.setName(aesUtils.decrypt(favorite.getName()));  
+         decryptedFavorite.setExternalID(aesUtils.decrypt(favorite.getExternalID())); 
+         return decryptedFavorite;
+     }).collect(Collectors.toSet());
     	
         return UserReadDTO.builder()
                 .id(user.getId())
@@ -34,7 +44,7 @@ public class CustomUserMapper {
                 .maxSearchDistance(user.getMaxSearchDistance())
                 .preferredCrowdLevel(user.getPreferredCrowdLevel())
                 .preferences(decryptedPreferences)
-                .favorites(user.getFavorites())
+                .favorites(decryptedFavorites)
                 .build();
     }
 
